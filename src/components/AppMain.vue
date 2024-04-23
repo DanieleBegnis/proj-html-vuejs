793948ce4aa59a28d84d310f89a1c1563416cae6
DanieleBegnis <daniele.begnis@gmail.com>
2024-04-23 16:49:03 +0200

    feat add first section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,13 +10,21 @@ export default {
 
 <template>
     <Hero></Hero>
-    <section>
+    <section class="about">
         <div class="container">
             <div class="row">
                 <div class="col-5">
-                    <img src="" alt="">
+                    <img class="py-5" src="../assets/img/about.png" alt="">
                 </div>
-                <div class="col-7"></div>
+                <div class="col-7">
+                    <div class="d-flex py-5">
+                        <img src="../assets/img/dog-icon.png" alt="">
+                        <p>About Us</p>
+                    </div>
+                    <h3>Welcome DogMilo Pets</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt consectetur id, accusantium quod deserunt delectus, quisquam, dolore atque dolorem porro nemo? Ad ut distinctio repellat illum vel animi incidunt! Aut.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt consectetur id, accusantium quod deserunt delectus, quisquam, dolore atque dolorem porro nemo? Ad ut distinctio repellat illum vel animi incidunt! Aut.</p>
+                </div>
             </div>
         </div>
     </section>
@@ -25,4 +33,11 @@ export default {
 <style scoped lang="scss">
 @use '../style/partials/variables' as *;
 
+.about {
+    .col-5 {
+        img {
+            height: 80%;
+        }
+    }
+}
 </style>
