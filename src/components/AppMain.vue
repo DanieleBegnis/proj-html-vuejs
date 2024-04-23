89191bd1f5ab4ef4c5d1bfdde3507ca1e837f056
DanieleBegnis <daniele.begnis@gmail.com>
2024-04-23 21:43:47 +0200

    feat: add card section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,38 @@
 <script>
 import Hero from './Hero.vue';
+import MainCards from './MainCards.vue';
 export default {
     name: 'AppMain',
     components: {
-        Hero
+        Hero,
+        MainCards
     },
+    data() {
+        return {
+            cards: [
+                {
+                    image: 'src/assets/img/d-1.png',
+                    title: 'Pet Sitting'
+                },
+                {
+                    image: 'src/assets/img/d-2.png',
+                    title: 'Puppy Sitting'
+                },
+                {
+                    image: 'src/assets/img/d-3.png',
+                    title: 'Dog Walking'
+                },
+                {
+                    image: 'src/assets/img/d-4.png',
+                    title: 'Night Care'
+                },
+                {
+                    image: 'src/assets/img/d-5.png',
+                    title: 'nail Trimming'
+                }
+            ]
+        }
+    }
 }
 </script>
 
@@ -19,7 +47,7 @@ export default {
                 <div class="col-7">
                     <div class="d-flex py-5">
                         <img src="../assets/img/dog-icon.png" alt="">
-                        <div>About Us</div>
+                        <div class="subtitle">About Us</div>
                     </div>
                     <h3>Welcome DogMilo Pets</h3>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt consectetur id, accusantium
@@ -29,6 +57,24 @@ export default {
                         quod deserunt delectus, quisquam, dolore atque dolorem porro nemo? Ad ut distinctio repellat
                         illum vel animi incidunt! Aut.</p>
                 </div>
+            </div>
+        </div>
+    </section>
+    <section class="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center">
+                    <p class="my-4">Service</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center">
+                    <h3>DogMilo Pet Care Services <br>
+                    For Your Best Friends.</h3>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <MainCards v-for="card in cards" :cardInfo="card"></MainCards>
             </div>
         </div>
     </section>
@@ -43,12 +89,22 @@ export default {
     background-size: 10%;
     background-position: right;
     background-position-y: 40px;
+    max-height: 600px;
 
     .col-5 {
         img {
             height: 80%;
             vertical-align: middle;
         }
+    }
+    .subtitle {
+        color: $brand-secondary;
+    }
+}
+.services {
+    background-color: #f5f5f5;
+    p {
+        color: $brand-secondary;
     }
 }
 </style>
---
+++ src/components/MainCards.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'MainCards',
+    props: ['cardInfo']
+}
+</script>
+
+<template>
+    <div class="col-2">
+        <div class="card-image">
+            <img :src="cardInfo.image">
+        </div>
+        <h5> {{ cardInfo.title }} </h5>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, minima aut.</p>
+        <div class="next">
+            <img src="../assets/img/right.png" alt="">
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+
+.col-2 {
+    background-color: $brand-primary;
+    color: #fff;
+}
+</style>
